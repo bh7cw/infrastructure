57a57eab2285b6afe34c3f8038235d44d09cfb63
bh7cw <ibh7cw@gmail.com>
2020-11-01 11:51:54 -0500

    add codedeploy policies to modules/services

--- modules/services/vars.tf
+++ modules/services/vars.tf
@@ -175,6 +175,254 @@ variable "aws_iam_policy_attachment_name" {
 }
 
 # -------------------------------------------------------------------
+# codedeploy policies
+// CodeDeploy-EC2-S3 policy
+variable "aws_iam_codedeploy_ec2_s3_policy_name" {
+  type = string
+  default = "CodeDeploy-EC2-S3"
+}
+
+variable "aws_iam_codedeploy_ec2_s3_policy_description" {
+  type = string
+  default = "allows EC2 instances to read data from S3 buckets"
+}
+
+variable "aws_iam_codedeploy_ec2_s3_policy_content" {
+  type = string
+  default = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+              "*",
+              "*"
+              ]
+        }
+    ]
+}
+EOF
+}
+
+# GH-Upload-To-S3 policy
+variable "aws_iam_gh_upload_to_s3_policy_name" {
+  type = string
+  default = "GH-Upload-To-S3"
+}
+
+variable "aws_iam_gh_upload_to_s3_policy_description" {
+  type = string
+  default = "allows GitHub Actions to upload artifacts from latest successful build to dedicated S3 bucket used by CodeDeploy"
+}
+
+variable "aws_iam_gh_upload_to_s3_policy_content" {
+  type = string
+  default = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+# GH-Code-Deploy policy
+variable "aws_iam_gh-code-deploy-policy_name" {
+  type = string
+  default = "GH-Code-Deploy"
+}
+
+variable "aws_iam_gh-code-deploy-policy_description" {
+  type = string
+  default = "allows GitHub Actions to call CodeDeploy APIs to initiate application deployment on EC2 instances"
+}
+
+variable "aws_iam_gh-code-deploy-policy_content" {
+  type = string
+  default = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:RegisterApplicationRevision",
+        "codedeploy:GetApplicationRevision"
+      ],
+      "Resource": [
+        "arn:aws:codedeploy:us-east-1:918834676735:application:mywebapp"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:CreateDeployment",
+        "codedeploy:GetDeployment"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:GetDeploymentConfig"
+      ],
+      "Resource": [
+        "arn:aws:codedeploy:us-east-1:918834676735:deploymentconfig:CodeDeployDefault.OneAtATime",
+        "arn:aws:codedeploy:us-east-1:918834676735:deploymentconfig:CodeDeployDefault.HalfAtATime",
+        "arn:aws:codedeploy:us-east-1:918834676735:deploymentconfig:CodeDeployDefault.AllAtOnce"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+//gh-ec2-ami policy
+variable "aws_iam_gh_ec2_ami_policy_name" {
+  type = string
+  default = "gh-ec2-ami"
+}
+
+variable "aws_iam_gh_ec2_ami_policy_description" {
+  type = string
+  default = "allows GitHub Actions to access to ec2"
+}
+
+variable "aws_iam_gh_ec2_ami_policy_content" {
+  type = string
+  default = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeyPair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+variable "ghaction_user_name" {
+  type = string
+  default = "ghactions"
+}
+
+variable "aws_iam_policy_attachment_gh_upload_to_s3_name" {
+  type = string
+  default = "ghactions_attach_gh_upload_to_s3"
+}
+
+variable "aws_iam_policy_attachment_gh_code_deploy_name" {
+  type = string
+  default = "ghactions_attach_gh_code_deploy"
+}
+
+variable "aws_iam_policy_attachment_gh_ec2_ami_name" {
+  type = string
+  default = "ghactions_attach_gh_ec2_ami"
+}
+
+//CodeDeployEC2ServiceRole
+variable "aws_code_deploy_iam_role_name" {
+  type = string
+  default = "CodeDeployEC2ServiceRole"
+}
+
+variable "aws_code_deploy_iam_role_assume_role_policy" {
+  type = string
+  default = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+EOF
+}
+
+variable "aws_iam_policy_attachment_code_deploy_ec2_name" {
+  type = string
+  default = "code_deploy_iam_role_attach"
+}
+
+//CodeDeployServiceRole
+variable "aws_code_deploy_service_role_name" {
+  type = string
+  default = "code_deploy_service_role_attach"
+}
+
+variable "aws_code_deploy_role" {
+  type = string
+  default = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+variable "aws_iam_policy_attachment_code_deploy_service_name" {
+  type = string
+  default = "code_deploy_service_role_attach"
+}
+
+# -------------------------------------------------------------------
 # aws_iam_instance_profile
 variable "aws_iam_instance_profile_name" {
   type = string
@@ -252,7 +500,7 @@ variable "aws_key_pair_key" {
 
 # -------------------------------------------------------------------
 # ec2 instance
-variable "aws_iam_instance_profile_name" {
+variable "aws_iam_ec2_instance_profile_name" {
   type = string
   default = "instance_profile"
 }
@@ -296,14 +544,19 @@ echo export BUCKET_NAME="webapp.jing.zhang" >> /etc/profile
 
 # -------------------------------------------------------------------
 # DNS record of ec2 public ip
-variable "dns_a_record_zone" {
-  type = string
-  default = "api."+"${env.AWS_PROFILE}"+".bh7cw.me."
-}
-
 variable "dns_a_record_name" {
   type = string
-  default = "dns_a_record_ec2_public_ip"
+  default = "api.dev.bh7cw.me."
+}
+
+variable "dns_a_record_type" {
+  type = string
+  default = "A"
+}
+
+variable "dns_a_record_ttl" {
+  type = string
+  default = "60"
 }
 
 # -------------------------------------------------------------------
